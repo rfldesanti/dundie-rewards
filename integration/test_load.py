0186dcb72e0ef6585e14760c908dbef1723cf518
Rafael <rafaelsr2018@gmail.com>
2022-10-22 16:46:31 -0300

    Add line length at .toml file

--- integration/test_load.py
+++ integration/test_load.py
@@ -20,7 +20,7 @@ def test_load_positive_call_load_command():
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """Test command load"""
     with pytest.raises(CalledProcessError) as error:
-        check_output(["dundie", wrong_command, "tests/assets/people.csv"]).decode(
-            "utf-8"
-        ).split("\n")
+        check_output(
+            ["dundie", wrong_command, "tests/assets/people.csv"]
+        ).decode("utf-8").split("\n")
     assert "status 2" in str(error.getrepr())
