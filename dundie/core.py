d669174057ce3756b99ede42deb493574b73b6b5
Rafa <60904413+rfldesanti@users.noreply.github.com>
2022-10-22 16:50:09 -0300

    add linters (#15)
    * add linters
    
    * add black
    
    * Add line length at .toml file

--- dundie/core.py
+++ dundie/core.py
@@ -3,9 +3,10 @@ from dundie.utils.log import get_logger
 
 log = get_logger()
 
+
 def load(filepath):
     """Loads data from filepath to the database
-    
+
     >>> len(load("assets/people.csv"))
     2
     >>> load("assets/people.csv")[0][0]
@@ -13,7 +14,7 @@ def load(filepath):
     """
     try:
         with open(filepath) as file_:
-           return [line.strip() for line in file_.readlines()]
+            return [line.strip() for line in file_.readlines()]
 
     except FileNotFoundError as e:
         log.error(str(e))
